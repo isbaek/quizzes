a6f8e0f4bf0e7a415d573054f64b6a1f3ce22fa2
Inseo Baek <yunibaek@hotmail.com>
2018-09-12 20:21:37 -0400

    UI: add quiz progress indicator

--- src/components/quiz.js
+++ src/components/quiz.js
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import LinearProgress from '@material-ui/core/LinearProgress';
+
 import Question from './question';
 
 class Quiz extends React.Component {
@@ -11,10 +13,16 @@ class Quiz extends React.Component {
     return this.props.quiz.answers.length + 1;
   }
 
+  quizProgress() {
+    const { questions, answers } = this.props.quiz;
+    return 100 * (answers.length / questions.length);
+  }
+
   render() {
     const { questions, answers } = this.props.quiz;
     return (
       <div>
+        <LinearProgress variant="determinate" value={this.quizProgress()} />
         <Question
           num={this.questionNum()}
           question={this.currentQuestion()}
