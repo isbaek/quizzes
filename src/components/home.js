fb06b2317b54b1b76c5426f9023741c0324efd02
Inseo Baek <yunibaek@hotmail.com>
2018-09-13 18:44:41 -0400

    UI: got rid of es lint error

--- src/components/home.js
+++ src/components/home.js
@@ -7,7 +7,6 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
-import Typography from '@material-ui/core/Typography';
 
 import Header from './header';
 
